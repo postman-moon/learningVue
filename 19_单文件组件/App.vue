0c8c49e626ca2f07dad7ed1a0fa895bae25d4a92
万道康 <wankang1998@gmail.com>
2022-06-19 16:03:26 +0800

    单文件组件

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <School></School>
+    <Studen></Studen>
+  </div>
+</template>
+
+<script>
+import School from './School.vue';
+import Student from './Student.vue';
+
+export default {
+  name: 'App',
+  components: {
+    School,
+    Student
+  }
+}
+</script>
+
+<style>
+
+</style>
