b7ebc53e7424e26c81c0f32672f874c8a2577415
万道康 <wankang1998@gmail.com>
2022-06-26 23:40:27 +0800

    todoList

---
+++ vue_test/06_src_scoped样式/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <School></School>
+
+    <hr>
+
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+import School from './components/School.vue'
+import Student from './components/Student.vue'
+
+export default {
+  name: 'App',
+  components: {
+    School,
+    Student,
+  },
+}
+</script>
