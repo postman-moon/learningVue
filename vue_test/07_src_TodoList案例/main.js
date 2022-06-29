542772599c7997f4129c3e18c7c4556d3406e288
万道康 <wankang1998@gmail.com>
2022-06-29 23:18:50 +0800

    本地存储

---
+++ vue_test/07_src_TodoList案例/main.js
@@ -0,0 +1,13 @@
+// 引入 Vue
+import Vue from 'vue';
+// 引入 App
+import App  from './App.vue';
+
+// 关闭 Vue 的生产提示
+Vue.config.productionTip =  false;
+
+// 创建 Vue 实例
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
