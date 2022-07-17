23488392f88a2c6979dedb6a68cee86bee22f13a
wandaokang <wandaokang>
2022-07-17 14:06:01 +0800

    事件总线

---
+++ vue_test/src/components/Student.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="student">
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Student',
+  data() {
+    return {
+      name: '张三',
+      sex: '男'
+    }
+  },
+  mounted() {
+    // console.log('Student', this.x);
+
+    this.x.$on('hello', (data) => {
+      console.log('我是School组件，收到了数据', data)
+    })
+  },
+}
+</script>
+
+<style scoped>
+  .student {
+    background-color: orange;
+    padding: 5px;
+    margin-top: 30px;
+  }
+</style>
--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -6,8 +6,15 @@ import App  from './App.vue';
 // 关闭 Vue 的生产提示
 Vue.config.productionTip =  false;
 
+// Vue.prototype.x = { a:1, b:2 }
+
+const Demo = Vue.extend();
+const d = new Demo();
+Vue.prototype.x = d;
+
+
 // 创建 Vue 实例
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
 })
