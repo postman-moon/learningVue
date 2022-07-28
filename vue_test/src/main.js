a4bccb28feee86e1570fe18163abb8e0f30d5a17
万道康 <wankang1998@gmail.com>
2022-07-29 00:16:27 +0800

    TodoList_pubsub版

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -1,20 +1,15 @@
-// 引入 Vue
-import Vue from 'vue';
-// 引入 App
-import App  from './App.vue';
+//引入Vue
+import Vue from 'vue'
+//引入App
+import App from './App.vue'
+//关闭Vue的生产提示
+Vue.config.productionTip = false
 
-// 关闭 Vue 的生产提示
-Vue.config.productionTip =  false;
-
-// Vue.prototype.x = { a:1, b:2 }
-
-/* const Demo = Vue.extend();
-const d = new Demo();
-Vue.prototype.x = d; */
-
-
-// 创建 Vue 实例
+//创建vm
 new Vue({
-  el: '#app',
-  render: h => h(App),
+	el:'#app',
+	render: h => h(App),
+	beforeCreate() {
+		Vue.prototype.$bus = this
+	},
 })
