e7bba51618b139356ba69b53feefce7263c648a0
万道康 <wankang1998@gmail.com>
2022-07-31 16:32:18 +0800

    TodoList_动画

---
+++ vue_test/17_src_过度与动画/components/Test2.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition name="hello" appear>
+      <h1 v-show="isShow">你好啊！</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data() {
+    return {
+      isShow: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  background-color: orange;
+}
+
+/* 进入的起点、离开的终点 */
+.hello-enter,
+.hello-leave-to {
+  transform: translateX(-100%);
+}
+
+.hello-enter-active,
+.hello-leave-active {
+  transition: 0.5s linear;
+}
+
+/* 进入的终点、离开的起点 */
+.hello-enter-to,
+.hello-leave {
+  transform: translateX(0);
+}
+
+
+</style>
