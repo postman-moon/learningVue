4f14d028376fab5332d50f2ff5b84a03b8baf9e8
万道康 <wankang1998@gmail.com>
2022-08-03 00:08:45 +0800

    配置代理服务器

--- vue_test/vue.config.js
+++ vue_test/vue.config.js
@@ -9,8 +9,20 @@ module.exports = {
   // 关闭语法检查
   lintOnSave: false,
 
-  // 开启代理服务器
-  devServer: {
+  // 开启代理服务器（方式一）
+  /* devServer: {
     proxy: 'http://localhost:5000'
+  } */
+
+  // 开启代理服务器（方式二）
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        pathRewrite: {'^/api': ''},
+        ws: true, // 用于支持 websocket
+        changeOrigin: true, // 用于控制请求头中的 host 值
+      },
+    }
   }
 }
