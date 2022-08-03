99dc59f8243dd33b891123c6c39baf4c09cd86d7
wandaokang <wandaokang>
2022-08-03 17:59:21 +0800

    github搜索案例

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-	<div id="root">
-		<button @click="getStudents">获取学生信息</button>
+	<div class="container">
+
+		<!-- 搜索区域 -->
+		<Search />
+
+		<!-- 数据展示区 -->
+		<List />
 	</div>
 </template>
 
 <script>
 import axios from 'axios';
+import Search from './components/Search';
+import List from './components/List';
 
-	export default {
-		name:'App',
-		methods: {
-			getStudents() {
-				// axios.get('http://localhost:8080/students').then(
-				axios.get('http://localhost:8080/api/students').then(
-					response => {
-						console.log('请求成功了', response.data);
-					},
-
-					error => {
-						console.log('请求失败了', error.message)
-					}
-				)
-			}
-		}
+export default {
+	name:'App',
+  components: { 
+		Search,
+		List,
+	},
+	methods: {
+		
 	}
+}
 </script>
-
