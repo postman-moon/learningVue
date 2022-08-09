1bcbbcf07bccb1c17ea9851fea8b0d7e7f1518b5
万道康 <wankang1998@gmail.com>
2022-08-10 00:43:01 +0800

    求和案例_vuex模块化编码

---
+++ vue_test/29_src_求和案例_vuex模块化编码/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="container">
+		<Count />
+
+		<hr>
+
+		<Person />
+	</div>
+</template>
+
+<script>
+import Count from './components/Count';
+import Person from './components/Person';
+
+export default {
+	name:'App',
+  components: { 
+		Count,
+		Person,
+	},
+	mounted() {
+		console.log('App', this);
+	}
+}
+</script>
