f80c58756a9c200625ced7dacf8dcc7b2ebe4bd7
万道康 <wankang1998@gmail.com>
2022-08-11 00:15:05 +0800

    路由的params参数

---
+++ vue_test/34_src_路由的params参数/pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <h2>我是Home的内容</h2>
+      <div>
+        <ul class="nav nav-tabs">
+          <li>
+            <!-- <a class="list-group-item" href="./home-news.html">News</a> -->
+            <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+          </li>
+          <li>
+            <!-- <a class="list-group-item active" href="./home-message.html">Message</a> -->
+            <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+
+          </li>
+        </ul>
+
+        <router-view></router-view>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Home',
+  /* beforeDestroy() {
+    console.log('Home组件即将被销毁了');
+  }, 
+  mounted() {
+    console.log('Home组件挂载完毕了', this);
+  },
+  */
+}
+</script>
