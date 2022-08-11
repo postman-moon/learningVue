44908f2ef8534db8cbf62a37d41cb6959384456f
万道康 <wankang1998@gmail.com>
2022-08-12 00:12:47 +0800

    组件内路由守卫

---
+++ vue_test/42_src_组件内路由守卫/pages/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <h2>我是Home的内容</h2>
+      <div>
+        <ul class="nav nav-tabs">
+          <li>
+            <!-- <a class="list-group-item" href="./home-news.html">News</a> -->
+            <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+          </li>
+          <li>
+            <!-- <a class="list-group-item active" href="./home-message.html">Message</a> -->
+            <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+
+          </li>
+        </ul>
+        <!-- 缓存多个路由组件 -->
+        <!-- <keep-alive :include="['News', 'Message']"> -->
+
+        <!-- 缓存一个路由组件 -->
+        <keep-alive include="News">
+          <router-view></router-view>
+        </keep-alive>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Home',
+  /* beforeDestroy() {
+    console.log('Home组件即将被销毁了');
+  }, 
+  mounted() {
+    console.log('Home组件挂载完毕了', this);
+  },
+  */
+}
+</script>
