0e75559395689edb6fcfe82c25c21a3830b7ff1d
wandaokang <wandaokang>
2022-08-12 11:18:36 +0800

    分析工程结构

---
+++ vue3_test/01_src_分析工程结构/main.js
@@ -0,0 +1,11 @@
+// 引入的不再是 Vue 构造函数了，引入的是一个名为 createApp 的工厂函数
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 创建应用实例对象 ———— app（类似于之前Vue2中的vm，但app比vm更“轻”）
+const app = createApp(App);
+
+console.log('@@@@', app);
+
+// 挂载
+app.mount('#app');
