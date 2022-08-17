f3c4ddc72d69969d6e49308ca2ae3baad658e632
wandaokang <wandaokang>
2022-08-17 15:20:48 +0800

    Teleport

---
+++ vue3_test/20_src_Teleport/components/Son.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="son">
+    <h3>我是Son组件（子）</h3>
+    <Dialog />
+  </div>
+</template>
+
+<script>
+  import Dialog from './Dialog';
+
+  export default {
+    name: 'Son',
+    components: {
+      Dialog,
+    }
+  }
+</script>
+
+<style>
+  .son {
+    background-color: orange;
+    padding: 10px;
+  }
+</style>
